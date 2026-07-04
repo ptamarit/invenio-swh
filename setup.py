78e65cf2e835526c2b51d5140b4969d612cfd7ba
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-07-04 23:28:18 +0200

    chore(setup): migrate from setuptools to hatchling
    * remove MANIFEST.in because that's only relevant for setuptools
    * declare conflicting extras for uvnINSERT
    * format pyproject.toml with Tombi
    
    Co-authored-by: Sam Arbid <samaa2@kth.se>

--- setup.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: 2020 CERN
-# SPDX-FileCopyrightText: 2020 Cottage Labs LLP.
-# SPDX-License-Identifier: MIT
-
-"""Support for onward deposit of software artifacts to Software Heritage."""
-from setuptools import setup
-
-setup()
